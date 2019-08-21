3e1d5cb616743444ba8f3c7d87a65e7b8764c59e
khalifak <54364823+khalifak@users.noreply.github.com>
2019-08-21 10:34:35 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/********************************************************************* Hello World
+********************************************************************** Saying hello to world
+********************************************************************** Khadija Khalifa 
+* 08/21/19
+* CMSC-255-0304
+********************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+System.out.println("Khadija");
+System.out.println("Hello World");
+System.out.println("Saying hello to world");
+System.out.println("CMSC-255-0304");
+System.out.println();
+	}
+	
+	public static void main(String [] args) {
+	 printHeading();
+	 System.out.println("Hello");
+	}
+}
